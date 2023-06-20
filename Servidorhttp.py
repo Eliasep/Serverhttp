6e1b152ca9cdd005a83e8b7715019ca977498028
Eliasep <91705138+Eliasep@users.noreply.github.com>
2023-06-20 08:16:20 -0300

    Add files via upload

--- Servidorhttp.py
+++ Servidorhttp.py
@@ -1,14 +1,14 @@
 import socket
 import os
+import threading
 
-HOST = 'localhost'
+HOST = '172.18.185.44'
 PORT = 8000
 
 Diretoriodestino = r'C:\\Users\\elias\\OneDrive\\Área de Trabalho\\Testepservidor'
 Tipospermitidos = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.otf', '.txt', '.html']
 
 def requisicao(request):
-    # Analisa a solicitação HTTP para obter o path do arquivo solicitado
     request_lines = request.split('\r\n')
 
     if request_lines:
@@ -18,15 +18,12 @@ def requisicao(request):
         if len(parts) == 3:
             metodo, path, protocolo = parts
         else:
-            # Gera uma resposta para solicitação inválida
             resposta = 'HTTP/1.1 400 Bad Request\r\nContent-Type: text/html\r\n\r\n<h1>Solicitação inválida</h1>'
             return resposta
     if metodo == 'GET':
         if path == '/':
-            # Obtém a lista de arquivos no diretório especificado
             entries = os.listdir(Diretoriodestino)
 
-            # Cria uma lista de links para os arquivos permitidos
             file_links = []
             for entry in entries:
                 entry_path = os.path.join(Diretoriodestino, entry)
@@ -35,11 +32,10 @@ def requisicao(request):
                     if extension in Tipospermitidos:
                         file_links.append(f'<li><a href="/download/{entry}">{entry}</a></li>')
 
-            # Gera a resposta HTML com os links de download dos arquivos
             conteudo_resposta = f'''
             <html>
             <body>
-                <h1>Arquivos Disponíveis para Download</h1>
+                <h1>Arquivos a Serem Visualizados</h1>
                 <ul>
                     {''.join(file_links)}
                 </ul>
@@ -47,81 +43,70 @@ def requisicao(request):
             </html>
             '''
 
-            # Gera a resposta HTTP
             resposta = f'HTTP/1.1 200 OK\r\nContent-Type: text/html\r\nContent-Length: {len(conteudo_resposta)}\r\n\r\n{conteudo_resposta}'
         elif path.startswith('/download/'):
-            # Obtém o nome do arquivo solicitado
             nome_arquivo = path.split('/')[-1]
             lista_links = os.path.join(Diretoriodestino, nome_arquivo)
 
-            # Verifica se o arquivo existe no diretório especificado
             if os.path.isfile(lista_links):
-                # Gera a resposta HTTP com o cabeçalho de download
                 resposta = f'HTTP/1.1 200 OK\r\nContent-Disposition: attachment; filename={nome_arquivo}\r\n\r\n'
             else:
-                # Gera uma resposta para arquivo não encontrado
                 resposta = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<h1>Arquivo não encontrado</h1>'
         elif path == '/HEADER':
-            # Gera a resposta HTTP com o cabeçalho da requisição
             cabecalhos = '\r\n'.join(request_lines[1:])
             resposta = f'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n{cabecalhos}'
         else:
-            # Gera uma resposta para path inválido
             resposta = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n<h1>path inválido</h1>'
     else:
-        # Gera uma resposta para outros métodos HTTP não suportados
         resposta = 'HTTP/1.1 405 metodo Not Allowed\r\nContent-Type: text/html\r\n\r\n<h1>Método não permitido</h1>'
 
     return resposta
 
+def handle_client(client_socket, client_address):
+    print(f'Conexão estabelecida: {client_address[0]}:{client_address[1]}')
+
+    requisitar_dados = client_socket.recv(1024).decode('utf-8')
+    print(f'Dados da solicitação:\n{requisitar_dados}')
+
+    resposta = requisicao(requisitar_dados)
+
+    client_socket.sendall(resposta.encode('utf-8'))
+
+    if resposta.startswith('HTTP/1.1 200 OK\r\nContent-Disposition: attachment; filename='):
+        nome_arquivo = resposta.split('\r\n\r\n')[0].split('filename=')[1]
+        lista_links = os.path.join(Diretoriodestino, nome_arquivo)
+
+        if os.path.isfile(lista_links):
+            with open(lista_links, 'rb') as file:
+                file_data = file.read()
+
+            client_socket.sendall(file_data)
+
+    client_socket.close()
+
 def run():
-    # Cria um soquete de servidor TCP
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         server_socket.bind((HOST, PORT))
-        server_socket.listen(5)  # Aumentamos o valor para aceitar múltiplas conexões
+        server_socket.listen(5)
         print(f'Servidor rodando em http://{HOST}:{PORT}')
 
-        conexoes = []  # Lista para armazenar as conexões estabelecidas
+        conexoes = []
 
         while True:
-            # Aguarda e aceita uma conexão do cliente
             client_socket, client_address = server_socket.accept()
-            print(f'Conexão estabelecida: {client_address[0]}:{client_address[1]}')
 
-            # Adiciona a conexão à lista de conexões estabelecidas
-            conexoes.append(client_socket)
+            client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
+            client_thread.start()
 
-            # Recebe os dados da solicitação do cliente
-            requisitar_dados = client_socket.recv(1024).decode('utf-8')
-            print(f'Dados da solicitação:\n{requisitar_dados}')
+            conexoes.append(client_thread)
 
-            # Processa a solicitação e obtém a resposta
-            resposta = requisicao(requisitar_dados)
+        for client_thread in conexoes:
+            client_thread.join()
 
-            # Envia a resposta ao cliente
-            client_socket.sendall(resposta.encode('utf-8'))
-
-            # Se o path de download foi solicitado, envia o arquivo
-            if resposta.startswith('HTTP/1.1 200 OK\r\nContent-Disposition: attachment; filename='):
-                nome_arquivo = resposta.split('\r\n\r\n')[0].split('filename=')[1]
-                lista_links = os.path.join(Diretoriodestino, nome_arquivo)
-
-                if os.path.isfile(lista_links):
-                    with open(lista_links, 'rb') as file:
-                        file_data = file.read()
-
-                    client_socket.sendall(file_data)
-
-            # Encerra a conexão com o cliente
-            client_socket.close()
-
-            # Remove a conexão da lista após encerrá-la
-            conexoes.remove(client_socket)
-
-        # Exibe as conexões ativas no terminal
         print(f'Conexões ativas: {len(conexoes)}')
-        for connection in conexoes:
-            print(f'Conexão: {connection.getpeername()[0]}:{connection.getpeername()[1]}')
+        for client_thread in conexoes:
+            client_socket = client_thread.args[0]
+            print(f'Conexão: {client_socket.getpeername()[0]}:{client_socket.getpeername()[1]}')
 
 run()
